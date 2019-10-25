befab65e8e3c99a0c53db7058fcd441e016e9abb
Aaron Southammavong <aaronsouthammavong@Aarons-MacBook-Pro.local>
2019-10-25 11:13:20 -0700

    Completed functional for queue.js and stack.js

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,26 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var front = 0;
+  var end = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[end] = value;
+    end++
   };
 
   someInstance.dequeue = function() {
+    var deq = storage[front];
+    delete storage[front];
+    front++;
+    return deq;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
+    // return end - front;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -6,12 +6,24 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
+    // add key-value pairs to storage object, 
+    // "[someInstance.size]" as the current length of storage, should be used as a key; will increment as values get pushed
+    // "storage[someInstance.size]" should be assigned the new value
+    storage[someInstance.size(storage)] = value;
+    // console.log('storage: ', storage);
+    // console.log('some instance.size at storage: ', someInstance.size(storage))
   };
 
   someInstance.pop = function() {
+    // console.log('some instance.size: ', someInstance.size())
+    // pop storage[at numerical string as key] and it's value
+    var popped = storage[someInstance.size()-1];
+    delete storage[someInstance.size()-1];
+    return popped;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
