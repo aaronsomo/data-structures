5824af7ceb4b14b69e6b0920b882b2c79a6d8dc0
Aaron Southammavong <aaronsouthammavong@Aarons-MacBook-Pro.local>
2019-10-25 11:14:03 -0700

    Completed functional-shared for queue.js and stack.js

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,46 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  // var someInstance = {};
+
+  var queue = {};
+  // Use an object with numeric keys to store values
+  queue.storage = {};
+  queue.front = 0;
+  queue.end = 0;
+
+  // Implement the methods below
+  _.extend(queue, queueMethods);
+
+  return queue;
 };
 
-var queueMethods = {};
+var queueMethods = {
+
+  // const _ = require('underscore');
+
+  enqueue(value) {
+    this.storage[this.end] = value;
+    this.end++;
+  },
+
+  dequeue(value) {
+    if (this.front < 0) {
+      this.front = 0;
+    }
+    var deq = this.storage[this.front];
+    delete this.storage[this.front];
+    this.front++;
+    return deq;
+  },
+
+  size(value) {
+    return Object.keys(this.storage).length;
+  }
+};
+
+
+
 
 
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,38 @@
+// const _ = require('underscore');
+
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  // var someInstance = {};
+
+  var stack = {};
+  // Use an object with numeric keys to store values
+  stack.storage = {};
+  stack.i = 0;
+
+  // Implement the methods below
+  _.extend(stack, stackMethods);
+
+  return stack;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push(value) {
+    this.storage[this.i] = value;
+    this.i++;
+  },
+
+  pop(value) {
+    this.i--;
+    if (this.i < 0) {
+      this.i = 0;
+    }
+    return this.storage[this.i];
+  },
+
+  size(value) {
+    return this.i;
+  }
+};
 
 
