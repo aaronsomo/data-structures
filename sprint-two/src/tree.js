828195c899a2a3b24e23107a92159ef9c0ae7339
Aaron Southammavong <aaronsouthammavong@Aarons-MacBook-Pro.local>
2019-10-25 19:21:35 -0700

    Completed tree.js

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,19 +1,34 @@
 var Tree = function(value) {
   var newTree = {};
+  newTree.childKey = 0;
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = {};
+
+  _.extend(newTree, treeMethods);
 
   return newTree;
 };
 
 var treeMethods = {};
-
-treeMethods.addChild = function(value) {
+  treeMethods.addChild = function(value) {
+  var childTree = Tree(value);
+  this.children[this.childKey] = childTree;
+  this.childKey++;
 };
 
 treeMethods.contains = function(target) {
+  var bool = false;
+  if (this.value === target) {
+    bool = true;
+  }
+  for (var i in this.children) {
+    if (this.children[i].contains(target)) {
+      bool = true;
+    }
+  }
+  return bool;
 };
 
 
