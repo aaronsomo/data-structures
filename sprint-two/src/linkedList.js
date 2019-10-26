29124c77dcf2804f28b75f85ea7dde9a24343e12
Aaron Southammavong <aaronsouthammavong@Aarons-MacBook-Pro.local>
2019-10-25 19:20:12 -0700

    Completed linkedList.js

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,41 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newNode = Node(value);
+    if (!list.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail = this.head;
+      if (this.tail.next) {
+        this.tail = this.tail.next;
+      }
+      this.tail.next = newNode;
+      this.tail = this.tail.next;
+    }
   };
 
   list.removeHead = function() {
+    if (this.head) {
+      var returnValue = this.head.value;
+      this.head = this.head.next;
+      return returnValue;
+    }
   };
 
   list.contains = function(target) {
+    var current;
+    if (this.head) {
+      current = this.head;
+      while (current) {
+        if (current.value === target) {
+          return true;
+        } else {
+          current = current.next;
+        }
+      }
+    }
+    return false;
   };
 
   return list;
