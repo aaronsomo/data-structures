463e6d11d58f38d767595a4423a5ebd8a3fe9243
Aaron Southammavong <aaronsouthammavong@Aarons-MBP.lan1>
2019-10-27 22:43:30 -0700

    Completes 5 instantiation patterns for queue and stack

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,30 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.count = 0;
+  }
+
+  enqueue(value) {
+    this.storage[this.count] = value;
+    this.count++;
+  }
+
+  dequeue() {
+    var deq = this.storage[0];
+    delete this.storage[0];
+    if (this.count < 1) {
+      return 0;
+    }
+    for (var i in this.storage) {
+      this.storage[i - 1] = this.storage[i];
+    }
+    this.count--;
+    return deq;
+  }
+
+  size() {
+    return this.count;
   }
 
 }
